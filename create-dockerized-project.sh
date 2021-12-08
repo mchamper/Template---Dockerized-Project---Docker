897f499fa6beaf0c1e7e24e3d989de68a89968d7
Marcelo Marrone <marcelochamper@outlook.com>
2021-12-08 18:01:19 -0300

    Correcciones varias

--- create-dockerized-project.sh
+++ create-dockerized-project.sh
@@ -5,17 +5,17 @@ rm -rf ./.tmp-dockerized-project/.git
 
 # CREATE
 if [ ! -f "$1/docker/version" ]; then
-    cp ./.tmp-dockerized-project/examples/.env.example ./.tmp-dockerized-project/.env
-    cp ./.tmp-dockerized-project/examples/compose.example.yml ./.tmp-dockerized-project/compose.dev.yml
-    cp ./.tmp-dockerized-project/examples/install.example.sh ./.tmp-dockerized-project/install.sh
+  cp ./.tmp-dockerized-project/examples/.env.example ./.tmp-dockerized-project/.env
+  cp ./.tmp-dockerized-project/examples/compose.example.yml ./.tmp-dockerized-project/compose.dev.yml
+  cp ./.tmp-dockerized-project/examples/install.example.sh ./.tmp-dockerized-project/install.sh
 
-    echo "/.env" > .gitignore
+  echo "/.env" > .gitignore
 
-    mkdir -p "$1/docker"
+  mkdir -p "$1/docker"
 
 # UPDATE
 else
-    rm -f ./.tmp-dockerized-project/.gitignore
+  rm -f ./.tmp-dockerized-project/.gitignore
 fi
 
 cp -a ./.tmp-dockerized-project/. "$1/docker"
