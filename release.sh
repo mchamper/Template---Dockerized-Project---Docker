9c8248a7fb6a3979ab66d4fc70a71a2d7f7f044f
Marcelo Marrone <marcelochamper@outlook.com>
2021-12-11 20:14:04 -0300

    Correcciones en release.sh

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ -f version ]]; then
-  $2 > version
+  $1 > version
 fi
 
 git pull --all
-git flow release start $2
-git flow release finish $2 -m
+git flow release start $1
+git flow release finish $1 -m
 git push --all
 git push --tags
