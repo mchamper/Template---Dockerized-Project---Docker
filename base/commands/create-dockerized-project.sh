c86f439009acc6d4450ff393ec2768b9ac4da463
Marcelo Marrone <marcelochamper@outlook.com>
2021-12-13 12:19:08 -0300

    Correcciones varias

--- base/commands/create-dockerized-project.sh
+++ base/commands/create-dockerized-project.sh
@@ -11,6 +11,7 @@ rm -rf ./.tmp-dockerized-project/.git
 # CREATE
 if [ ! -f "$path/version" ]; then
   cp ./.tmp-dockerized-project/examples/.example.env ./.tmp-dockerized-project/.env
+  cp ./.tmp-dockerized-project/examples/.example.env ./.tmp-dockerized-project/.env.example
   cp ./.tmp-dockerized-project/examples/compose.example.yml ./.tmp-dockerized-project/compose.dev.yml
   cp ./.tmp-dockerized-project/examples/install.example.sh ./.tmp-dockerized-project/install.sh
   cp ./.tmp-dockerized-project/examples/.database-exports.example.env ./.tmp-dockerized-project/database-exports/.env
