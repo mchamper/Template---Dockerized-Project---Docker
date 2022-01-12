7ee767ca436c3856cb927411bd7830960388011e
Marcelo Marrone <marcelochamper@outlook.com>
2022-01-12 16:56:24 -0300

    Corrección de comandos

--- base/bin/sources/remove.sh
+++ base/bin/sources/remove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f .env ]; then cd ../../; fi; . .env
-CMD=$1; ARG1=$2; ARG2=$3; ARG3=$4; ARG4=$5; $ARG5=$6
+CMD=$1; ARG1=$2; ARG2=$3; ARG3=$4; ARG4=$5; ARG5=$6;
 
 if [[ $CMD != "--exec" ]]; then
   for SRC in "${SRCS[@]}"; do
