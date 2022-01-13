f8f23b1db7d4b40552d2a05c287b7ffbb61d0cb5
Marcelo Marrone <marcelochamper@outlook.com>
2022-01-12 23:28:55 -0300

    Actualizado comando eb ssh para pasar comando

--- base/bin/aws/eb-ssh.sh
+++ base/bin/aws/eb-ssh.sh
@@ -4,4 +4,8 @@ if [ ! -f .env ]; then cd ../../; fi; . .env
 
 SERVICE=$1
 
-bash base/bin/docker/run.sh $SERVICE "eb ssh"
+if [[ $2 != "" ]]; then
+  bash base/bin/docker/run.sh $SERVICE "eb ssh -n 1 --command \"$2\""
+else
+  bash base/bin/docker/run.sh $SERVICE "eb ssh"
+fi
