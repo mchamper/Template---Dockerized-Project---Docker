e2acb7102c628edd372c8ebae0bdaad11013cca7
Marcelo Marrone <marcelochamper@outlook.com>
2022-01-16 11:40:50 -0300

    Mejorado comando aws s3 upload

--- base/bin/aws/s3-upload.sh
+++ base/bin/aws/s3-upload.sh
@@ -5,7 +5,12 @@ if [ ! -f .env ]; then cd ../../; fi; . .env
 SERVICE=$1
 SOURCE=$2
 S3_BUCKET=$3
+AWS_CMD="sync"
+
+if [[ $4 = "--delete" ]]; then
+  AWS_CMD="sync --delete"
+fi
 
 bash base/bin/docker/run.sh $SERVICE "
-  aws s3 cp $SOURCE s3://$S3_BUCKET --recursive --exclude \".git/*\";
+  aws s3 $AWS_CMD $SOURCE s3://$S3_BUCKET --exclude \".git/*\" --exclude \".aws.s3.*\";
 "
