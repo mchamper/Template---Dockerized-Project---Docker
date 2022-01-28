e4fde7170f2867612678fcb1b1f42eddc95fb39a
Marcelo Marrone <marcelochamper@outlook.com>
2022-01-28 16:31:21 -0300

    Actualizado add ip para elegir puerto según motor de db

--- base/bin/aws/ec2-add-ip.sh
+++ base/bin/aws/ec2-add-ip.sh
@@ -6,15 +6,17 @@ SERVICE=$1
 SECURITY_GROUP_ID=$2
 IP=$3
 DESCRIPTION=$4
+PORT=$5
 REGEX="^[0-9]+"
 
 if ! [[ $IP =~ $REGEX ]]; then
-  DESCRIPTION=$IP
   IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+  DESCRIPTION=$3
+  PORT=$4
 fi
 
 bash base/bin/docker/run.sh $SERVICE "
   aws ec2 authorize-security-group-ingress \
     --group-id $SECURITY_GROUP_ID \
-    --ip-permissions IpProtocol=tcp,FromPort=3306,ToPort=3306,IpRanges=[{CidrIp=$IP/32,Description=\"$DESCRIPTION\"}]
+    --ip-permissions IpProtocol=tcp,FromPort=$PORT,ToPort=$PORT,IpRanges=[{CidrIp=$IP/32,Description=\"$DESCRIPTION\"}]
 "
