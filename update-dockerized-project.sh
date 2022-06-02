6f1585e4fec8743651d2e514169bc3bec0ba55ff
Marcelo Marrone <marcelochamper@outlook.com>
2022-06-02 15:02:06 -0300

    Correcciones varias

--- update-dockerized-project.sh
+++ update-dockerized-project.sh
@@ -3,7 +3,7 @@
 git clone git@github.com:mchamper/Template---Dockerized-Project---Docker.git ./.tmp-dockerized-project || exit 1
 bash ./.tmp-dockerized-project/base/bin/create-dockerized-project.sh ./ --root
 
-if [[ ${1} == "--commit"]]; then
+if [[ ${1} == "--commit" ]]; then
   git add .
   git commit -m "Auto commit: Dockerized project updated"
   git push
