91487b6eb33314796b05426cde8576c978bbb9d7
Marcelo Marrone <marcelochamper@outlook.com>
2022-06-02 14:59:33 -0300

    Opcion commit para update de dockerized project

--- update-dockerized-project.sh
+++ update-dockerized-project.sh
@@ -2,3 +2,9 @@
 
 git clone git@github.com:mchamper/Template---Dockerized-Project---Docker.git ./.tmp-dockerized-project || exit 1
 bash ./.tmp-dockerized-project/base/bin/create-dockerized-project.sh ./ --root
+
+if [[ ${1} == "--commit"]]; then
+  git add .
+  git commit -m "Auto commit: Dockerized project updated"
+  git push
+fi
