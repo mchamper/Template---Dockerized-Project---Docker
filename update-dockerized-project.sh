6561a19d4a25b003c3171948faa5f011237920a1
Marcelo Marrone <marcelochamper@outlook.com>
2022-07-10 16:25:55 -0300

    Refactoring varios

--- update-dockerized-project.sh
+++ update-dockerized-project.sh
@@ -4,7 +4,5 @@ git clone git@github.com:mchamper/Template---Dockerized-Project---Docker.git ./.
 bash ./.tmp-dockerized-project/base/bin/create-dockerized-project.sh ./ --root
 
 if [[ ${1} == "--commit" ]]; then
-  git add .
-  git commit -m "Auto commit: Dockerized project updated"
-  git push
+  git add . && git commit -m "Auto commit: Dockerized project updated" && git push
 fi
