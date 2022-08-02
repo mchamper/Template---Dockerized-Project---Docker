ec659441da7f64f99011c485a23a8698fe378bd4
Marcelo Marrone <marcelochamper@outlook.com>
2022-08-02 15:50:03 -0300

    Agregado dockerfile para mysql

---
+++ base/dockerfiles/mysql/import-remote.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. ../exports/.env || exit 1
+
+FILE="../exports/dump.sql"
+
+echo "Importing..."
+mysql --host=${HOST} --port=${PORT} -u ${USER} -p"${PASSWORD}" ${DATABASE} < ${FILE}
+echo "Import finished"
