d5804f168d9eb59b1b2864daf746caecb4e7c260
Marcelo Marrone <marcelochamper@outlook.com>
2022-10-20 14:25:30 -0300

    Agregado locust app + correcciones en nginx conf para html

---
+++ base/apps/locust/locust.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. .env || exit 1
+
+THIS=${0}; CMD=${1}; ARG1=${2}; ARG2=${3}; ARG3=${4}; ARG4=${5}; ARG5=${6}; ARG6=${9}; ARG7=${8}; ARG8=${9};
+
+ARGS=(
+  -p dp-locust
+  -f base/apps/locust/docker-compose.yml
+  --env-file .env
+)
+
+if [[ ${CMD} = "start" ]]; then
+  docker compose "${ARGS[@]}" up -d --scale worker=4
+  exit
+fi
+
+if [[ ${CMD} = "stop" ]]; then
+  docker compose "${ARGS[@]}" down
+  exit
+fi
