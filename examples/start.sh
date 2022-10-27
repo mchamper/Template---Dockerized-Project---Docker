0dd3a567103ed2fe87489a67c58fd713f27e1a71
Marcelo Marrone <marcelochamper@outlook.com>
2022-10-27 10:27:06 -0300

    Correcciones varias

--- examples/start.sh
+++ examples/start.sh
@@ -2,4 +2,6 @@
 
 . .env || exit 1
 
-bash base/bin/docker/start.sh ${1}
+bash base/bin/docker/start.sh "${1}"
+
+# bash base/bin/docker/exec-d.sh web-dist-ssr ". /root/.bashrc && PORT=3000 node dist/app/server/main.js"
