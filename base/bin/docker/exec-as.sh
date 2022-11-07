4121cf7b70b7ad60fb536d07f95f949a1718d8ed
Marcelo Marrone <marcelochamper@outlook.com>
2022-11-07 10:06:09 -0300

    Actualizados dockerfiles para node con nginx

---
+++ base/bin/docker/exec-as.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. .env || exit 1
+
+SERVICE=${1}
+COMMAND=${2}
+USER=${3:-root}
+
+docker compose \
+  -f compose.${ENV}.yml \
+  exec -u ${USER} ${SERVICE} \
+  /bin/sh -c "${COMMAND}"
