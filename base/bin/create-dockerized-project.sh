4efb592445882e61c73493f73a9c9e196fa7ece3
Marcelo Marrone <marcelochamper@outlook.com>
2023-02-14 16:20:53 -0300

    Correcciones varias

---
+++ base/bin/create-dockerized-project.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+SOURCE=${1}
+
+if [[ ${2} != "--root" ]]; then
+  SOURCE="${1}/docker"
+fi
+
+rm -rf .tmp-dockerized-project/.git
+
+mkdir -p .tmp-dockerized-project/bin/core
+mkdir -p .tmp-dockerized-project/credentials/.aws
+mkdir -p .tmp-dockerized-project/credentials/.ssh
+
+# CREATE
+if [[ ! -f "${SOURCE}/version" && ! -f "${SOURCE}/version.txt" ]]; then
+  cp .tmp-dockerized-project/examples/.env .tmp-dockerized-project/.env
+  cp .tmp-dockerized-project/examples/.env .tmp-dockerized-project/.env.example
+  cp .tmp-dockerized-project/examples/compose.dev.yml .tmp-dockerized-project/compose.dev.yml
+  cp .tmp-dockerized-project/examples/install.sh .tmp-dockerized-project/install.sh
+  cp .tmp-dockerized-project/examples/build.sh .tmp-dockerized-project/build.sh
+  cp .tmp-dockerized-project/examples/start.sh .tmp-dockerized-project/start.sh
+  cp .tmp-dockerized-project/examples/stop.sh .tmp-dockerized-project/stop.sh
+  cp .tmp-dockerized-project/examples/database-exports/.env .tmp-dockerized-project/database-exports/.env
+  cp -r .tmp-dockerized-project/examples/bin/core .tmp-dockerized-project/bin
+  cp -r .tmp-dockerized-project/examples/credentials/.aws .tmp-dockerized-project/credentials
+
+  echo "/.env" > .tmp-dockerized-project/.gitignore
+
+  mkdir -p "${SOURCE}"
+
+# UPDATE
+else
+  cp .tmp-dockerized-project/examples/bin/core/__base.sh .tmp-dockerized-project/bin/core/__base.sh
+  rm -f .tmp-dockerized-project/.gitignore
+fi
+
+rm -rf .tmp-dockerized-project/downloads
+rm -f .tmp-dockerized-project/base/bin/create-dockerized-project.sh
+
+rm -rf "${SOURCE}"/base
+rm -rf "${SOURCE}"/examples
+
+cp -a .tmp-dockerized-project/. "${SOURCE}"
+rm -rf .tmp-dockerized-project
