fc3c489e7747bffd2620afae5d201498bdbf36e1
Marcelo Marrone <marcelochamper@outlook.com>
2023-03-23 16:37:39 -0300

    Agregada app para swagger

--- base/apps/swagger/swagger.sh
+++ base/apps/swagger/swagger.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+. .env || exit 1
+
 THIS=${0}; CMD=${1}; ARG1=${2}; ARG2=${3}; ARG3=${4}; ARG4=${5}; ARG5=${6}; ARG6=${9}; ARG7=${8}; ARG8=${9};
 
 ARGS=(
-  -p dp-locust
-  -f base/apps/locust/docker-compose.yml
-  --env-file .env
+  -p dp-swagger
+  -f base/apps/swagger/docker-compose.yml
+  --env-file base/apps/swagger/.env
 )
 
 if [[ ${CMD} = "start" ]]; then
-  docker compose "${ARGS[@]}" up -d --scale worker=4
+  echo "SWAGGER_JSON=${ARG1}" > base/apps/swagger/.env
+  docker compose "${ARGS[@]}" up -d
+
   exit
 fi
 
 if [[ ${CMD} = "stop" ]]; then
   docker compose "${ARGS[@]}" down
+  > base/apps/swagger/.env
+
   exit
 fi
