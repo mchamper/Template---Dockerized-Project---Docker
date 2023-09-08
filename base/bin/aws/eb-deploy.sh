46adf690e3b03d8f05ee906b569b045e75804dcb
Marcelo Marrone <marcelochamper@outlook.com>
2023-09-08 16:38:06 -0300

    Agregado comando branch + mejora en eb deploy

--- base/bin/aws/eb-deploy.sh
+++ base/bin/aws/eb-deploy.sh
@@ -3,9 +3,10 @@
 . .env || exit 1
 
 SERVICE=${1}
+BRANCH=$(bash base/bin/git/branch.sh ${SERVICE}) || exit 1
 VERSION=$(bash base/bin/git/version.sh ${SERVICE}) || exit 1
 VERSION_FULL=$(bash base/bin/git/version.sh ${SERVICE} --full) || exit 1
-LABEL="${COMPOSE_PROJECT_NAME}-${SERVICE}-${VERSION_FULL}"
+LABEL="${COMPOSE_PROJECT_NAME}-${SERVICE}-${BRANCH}-${VERSION_FULL}"
 
 if [[ ${2} = "-v" ]]; then
   echo ${LABEL}
