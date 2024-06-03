b76abeda7151315d0cdb7888677077a645593ade
Marcelo Marrone <marcelochamper@outlook.com>
2024-06-03 12:12:53 -0300

    Correcciones para install de frameworks

--- examples/bin/core/backend.sh
+++ examples/bin/core/backend.sh
@@ -40,33 +40,20 @@ if [[ ${CMD} = "deploy-and-migrate" ]]; then
 fi
 
 if [[ ${CMD} = "get-envs" ]]; then
-  # bash base/bin/aws/s3-getenvs.sh 0-utils "aws1-hoggax-environments/${SERVICE}-prod/.env" "environments/${SERVICE}-prod/.env[REMOTE]"
-
-  if [[ ${ARG1} = "--backup" ]]; then
-    # echo "$(bash ${THIS} ssh "cat /var/app/current/.env")" > "environments/environments/${SERVICE}-prod/.env[REMOTE]"
-    echo $(bash base/bin/aws/eb-printenv.sh ${SERVICE}) > "environments/${SERVICE}-prod/.env[REMOTE]"
-    exit
-  fi
-
-  bash base/bin/aws/eb-printenv.sh ${SERVICE}
+  bash base/bin/aws/s3-getenvs.sh 0-utils "aws-projectname-environments/${SERVICE}-prod/.env" "environments/${SERVICE}-prod/.env[REMOTE]"
   exit
 fi
 
 if [[ ${CMD} = "set-envs" ]]; then
-  # bash base/bin/aws/s3-setenvs.sh 0-utils "environments/${SERVICE}-prod/.env" "aws1-hoggax-environments/${SERVICE}-prod/.env"
-
-  # bash ${THIS} ssh "echo '$(cat environments/${SERVICE}-prod/.env)' > /var/app/current/.env"
-  bash base/bin/aws/eb-setenv.sh ${SERVICE} "environments/${SERVICE}-prod/.env"
-  bash ${THIS} deploy
-
+  bash base/bin/aws/s3-setenvs.sh 0-utils "environments/${SERVICE}-prod/.env" "aws-projectname-environments/${SERVICE}-prod/.env"
   exit
 fi
 
 if [[ ${CMD} = "logs" ]]; then
-  # echo "$(bash ${THIS} eb-ssh "cat /var/log/eb-engine.log")" > "logs/${SERVICE}-prod--aws-eb-engine.log"
+  echo "$(bash ${THIS} eb-ssh "cat /var/log/eb-engine.log")" > "logs/${SERVICE}-prod--aws-eb-engine.log"
   # echo "$(bash ${THIS} eb-ssh "cat /var/log/cfn-init.log")" > "logs/${SERVICE}-prod--aws-cfn-init.log"
-  # echo "$(bash ${THIS} eb-ssh "cat /var/log/cfn-init-cmd.log")" > "logs/${SERVICE}-prod--awl-cfn-init-cmd.log"
-  echo "$(bash ${THIS} eb-ssh "cat /var/app/current/storage/logs/laravel.log")" > "logs/${SERVICE}--aws--laravel.log"
+  # echo "$(bash ${THIS} eb-ssh "cat /var/log/cfn-init-cmd.log")" > "logs/${SERVICE}-prod--aws-cfn-init-cmd.log"
+  echo "$(bash ${THIS} eb-ssh "cat /var/app/current/storage/logs/laravel.log")" > "logs/${SERVICE}-prod--laravel.log"
 
   exit
 fi
