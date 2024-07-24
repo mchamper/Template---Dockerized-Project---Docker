9419a3877d19b740edf2d00475779a6d3ff29723
Marcelo Marrone <marcelochamper@outlook.com>
2024-07-24 14:10:37 -0300

    Correcciones en environment de angular

--- base/frameworks/angular/src/src/environments/environment.prod.ts
+++ base/frameworks/angular/src/src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment: TEnvironment = {
   mock: false,
   appUrl: 'https://example.com',
   backendUrl: 'https://backend.example.com',
+  authGoogleClientId: '',
   // gtmId: '',
   // fbPixelId: '',
 };
