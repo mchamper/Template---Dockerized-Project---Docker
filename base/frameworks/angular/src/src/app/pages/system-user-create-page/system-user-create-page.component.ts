1a3d3c5aea941b1cc8d4b452344385a6e3846a65
Marcelo Marrone <marcelochamper@outlook.com>
2024-07-24 23:41:49 -0300

    Correcciones varias

--- base/frameworks/angular/src/src/app/pages/system-user-create-page/system-user-create-page.component.ts
+++ base/frameworks/angular/src/src/app/pages/system-user-create-page/system-user-create-page.component.ts
@@ -33,67 +33,67 @@ export class SystemUserCreatePageComponent {
   private _systemUserHttpS = inject(SystemUserHttpService);
   authS = inject(AuthService);
 
-  form: Form;
+  form = new Form(this._fb.group({
+    first_name: this._fb.control('', { validators: [Validators.required] }),
+    last_name: this._fb.control('', { validators: [Validators.required] }),
+    email: this._fb.control('', { validators: [Validators.required, Validators.email] }),
+    password_input_type: this._fb.control<'random' | 'manual'>('random', { validators: [Validators.required] }),
+    password: this._fb.control('', { validators: [Validators.required] }),
+    password_confirmation: this._fb.control('', { validators: [Validators.required] }),
+    require_email_verification: this._fb.control(false, { validators: [Validators.required] }),
+  }), {
+    mock: systemUserCreateFormMock(),
+    subscriptions: (form) => {
+      form.changes('password_input_type').subscribe(value => {
+        if (value === 'random') {
+          form.group.controls.password.setValue('');
+          form.group.controls.password.disable();
+          form.group.controls.password_confirmation.setValue('');
+          form.group.controls.password_confirmation.disable();
+        } else {
+          form.group.controls.password.enable();
+          form.group.controls.password_confirmation.enable();
+        }
+
+        form.group.controls.password.updateValueAndValidity();
+        form.group.controls.password_confirmation.updateValueAndValidity();
+      });
+    },
+    request: {
+      send: (): any => this._systemUserHttpS.create(this.form.group.value),
+      success: (res) => {
+        this._nzNotificationS.success(
+          `<strong>Usuario creado</strong>`,
+          res.body.system_user_password
+            ? `
+              <p class="mb-2">El usuario se ha creado con éxito.</p>
+              <p class="mb-2">Se ha generado una contraseña aleatoria que se mostrará a continuación por única vez:<br><code>${escape(res.body.system_user_password)}</code></p>
+              ${
+                this.form.getValue('require_email_verification')
+                  ? '<p class="mb-0">También se le ha enviado un email de verificación a la dirección de correo ingresada.</p>'
+                  : ''
+              }
+            `
+            : `
+              <p class="mb-2">El usuario se ha creado con éxito.</p>
+              ${
+                this.form.getValue('require_email_verification')
+                  ? '<p class="mb-0">Se le ha enviado un email de verificación a la dirección de correo ingresada.</p>'
+                  : ''
+              }
+            `
+            ,
+          {
+            nzDuration: 5000,
+          }
+        );
+
+        this._router.navigate(['/usuarios']);
+      }
+    },
+  });;
 
   constructor() {
-    this.form = new Form(this._fb.group({
-      first_name: [null, [Validators.required]],
-      last_name: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.email]],
-      password_input_type: ['random', Validators.required],
-      password: [null, [Validators.required]],
-      password_confirmation: [null, [Validators.required]],
-      require_email_verification: [false, [Validators.required]],
-    }), {
-      mock: systemUserCreateFormMock(),
-      subscriptions: (form) => {
-        form.changes('password_input_type').subscribe(value => {
-          if (value === 'random') {
-            form.getControl('password')?.setValue('');
-            form.getControl('password')?.disable();
-            form.getControl('password_confirmation')?.setValue('');
-            form.getControl('password_confirmation')?.disable();
-          } else {
-            form.getControl('password')?.enable();
-            form.getControl('password_confirmation')?.enable();
-          }
-
-          form.getControl('password').updateValueAndValidity();
-          form.getControl('password_confirmation').updateValueAndValidity();
-        });
-      },
-      request: {
-        send: () => this._systemUserHttpS.create({ ...this.form.group.value }),
-        success: (res) => {
-          this._nzNotificationS.success(
-            `<strong>Usuario creado</strong>`,
-            res.body.system_user_password
-              ? `
-                <p class="mb-2">El usuario se ha creado con éxito.</p>
-                <p class="mb-2">Se ha generado una contraseña aleatoria que se mostrará a continuación por única vez:<br><code>${escape(res.body.system_user_password)}</code></p>
-                ${
-                  this.form.getValue('require_email_verification')
-                    ? '<p class="mb-0">También se le ha enviado un email de verificación a la dirección de correo ingresada.</p>'
-                    : ''
-                }
-              `
-              : `
-                <p class="mb-2">El usuario se ha creado con éxito.</p>
-                ${
-                  this.form.getValue('require_email_verification')
-                    ? '<p class="mb-0">Se le ha enviado un email de verificación a la dirección de correo ingresada.</p>'
-                    : ''
-                }
-              `
-              ,
-            {
-              nzDuration: 5000,
-            }
-          );
-
-          this._router.navigate(['/usuarios']);
-        }
-      },
-    });
+    //
   }
 }
