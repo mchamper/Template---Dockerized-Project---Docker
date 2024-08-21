977c6f1d5acf6952c48f759d27763919ef18f79f
Marcelo Marrone <marcelochamper@outlook.com>
2024-08-21 11:38:40 -0300

    Nuevo BaseRequest [laravel]

--- base/frameworks/laravel/src/app/Core/Bases/BaseRequest.php
+++ base/frameworks/laravel/src/app/Core/Bases/BaseRequest.php
@@ -5,11 +5,36 @@ namespace App\Core\Bases;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 // abstract class BaseRequest extends FormRequest
 abstract class BaseRequest
 {
     abstract public static function rules(?array $params = []);
+
+    /* -------------------- */
+
+    protected static function _replaceRule(array &$fieldRules, $oldRule, $newRule)
+    {
+        $ruleIndex = is_numeric($oldRule)
+            ? $oldRule
+            : array_search($oldRule, $fieldRules);
+
+        if ($ruleIndex !== false) {
+            unset($fieldRules[$ruleIndex]);
+            $fieldRules[$ruleIndex] = $newRule;
+
+            ksort($fieldRules);
+            $fieldRules = array_values($fieldRules);
+        }
+    }
+
+    /* -------------------- */
+
+    public static function rulesWithPrefix(string $prefix, ?array $params = [], )
+    {
+        return Arr::prependKeysWith(static::rules($params), "$prefix.");
+    }
 
     public static function rulesFrom($input, ?array $params = [])
     {
@@ -17,7 +42,7 @@ abstract class BaseRequest
             $arrayKeyExists = false;
 
             foreach ($input as $key2 => $value2) {
-                if ($arrayKeyExists = Str::startsWith($key, $key2 . '.')) {
+                if ($arrayKeyExists = Str::startsWith($key, "$key2.")) {
                     break;
                 }
             }
