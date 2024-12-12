99270b7c262d58c8494f93aeb3151823ed642baf
Marcelo Marrone <marcelochamper@outlook.com>
2024-12-12 10:51:17 -0300

    Agregado log basico para apis

--- base/frameworks/laravel/src/config/logging.php
+++ base/frameworks/laravel/src/config/logging.php
@@ -65,6 +65,13 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'api' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel-api.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
+        ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
